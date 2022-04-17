527076087ae908c7848eb35d40fd3bd09625c1c8
notin <chadvansyoc11@gmail.com>
2022-04-17 14:53:21 -0700

    updating pokemon

--- pokemon/src/App.tsx
+++ pokemon/src/App.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./index.css";
+import Pokemon from "./Pokemon";
 
 const App = () => (
-  <div className="container">
-    <div>Name: pokemon</div>
-    <div>Framework: react</div>
-    <div>Language: TypeScript</div>
-    <div>CSS: Empty CSS</div>
+    <div className="container">
+        <div>Name: pokemon</div>
+        <div>Framework: react</div>
+        <div>Language: TypeScript</div>
+        <div>CSS: Empty CSS</div>
+        <Pokemon></Pokemon>
   </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
---
+++ pokemon/src/contexts/pk.ts
@@ -0,0 +1,7 @@
+let pk = {
+    pokeName : "",
+    pokeAbilityUrls: [{name: "", url: ""}],
+    pokeFormUrl : "",
+    pokeMoveUrls: [{name: "", url: "", level_at:""}]
+}
+export default pk
