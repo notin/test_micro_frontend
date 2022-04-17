ac166d0e2ef9684ebe6ad48790ba50c94c4b8c93
jwindberg <john@windberg.name>
2022-04-16 21:28:27 -0700

    wip

--- amiibo_card/src/App.tsx
+++ amiibo_card/src/App.tsx
@@ -1,4 +1,6 @@
+// @ts-ignore
 import React from "react";
+// @ts-ignore
 import ReactDOM from "react-dom";
 import amiibo_cards_container from "./amiibo_cards_container"
 
--- amiibo_card/src/amiibo_cards_container.tsx
+++ amiibo_card/src/amiibo_cards_container.tsx
@@ -1,5 +1,17 @@
-import React from "react";
+// @ts-ignore
+import React, {useEffect, useState}  from "react";
 const amiibo_cards_container = () => {
+
+    // @ts-ignore
+    const fetchCards = async () => {
+        let response = await fetch("http://localhost:8080/api/amiiboCards");
+        let promise = await response.json();
+
+    }
+
+    useEffect( () => {
+        fetchCards().then();
+    }, [])
 
 }
 
