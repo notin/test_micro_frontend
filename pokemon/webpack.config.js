b00331e2d421c26e8094dc222824663ec5eb8d03
notin <chadvansyoc11@gmail.com>
2022-09-03 19:07:40 -0700

    test new endpount

--- pokemon/webpack.config.js
+++ pokemon/webpack.config.js
@@ -18,26 +18,37 @@ module.exports = {
   },
 
   module: {
-    rules: [
+
+    // rules: [
+    //   {
+    //     test: /\.m?js/,
+    //     type: "javascript/auto",
+    //     resolve: {
+    //       fullySpecified: false,
+    //     },
+    //   },
+    //   {
+    //     test: /\.(css|s[ac]ss)$/i,
+    //     use: ["style-loader", "css-loader", "postcss-loader"],
+    //   },
+    //   {
+    //     test: /\.(ts|tsx|js|jsx)$/,
+    //     exclude: /node_modules/,
+    //     use: {
+    //       loader: "babel-loader",
+    //     },
+    //   },
+    // ],
+    loaders: [
       {
-        test: /\.m?js/,
-        type: "javascript/auto",
-        resolve: {
-          fullySpecified: false,
-        },
-      },
-      {
-        test: /\.(css|s[ac]ss)$/i,
-        use: ["style-loader", "css-loader", "postcss-loader"],
-      },
-      {
-        test: /\.(ts|tsx|js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-    ],
+        test: /\.tsx?$/,
+        exclude:/(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
   },
 
   plugins: [
